a298f906c165abd2abafc04ef191db5fe0eaffaf
wjun <wjunjobs@outlook.com>
2022-11-28 00:01:25 +0800

    测试rust解析yaml文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate serde_yaml;
+extern crate serde;
+
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Server {
+    version: String,
+    services: HashMap<String, InnerServer>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct InnerServer {
+    env_file: Vec<String>,
+    image: String,
+    hostname: String,
+    container_name: String,
+    volumes: Vec<String>,
+    // ports: Vec<String>,
+    command: Vec<String>,
+}
+
 fn main() {
-    println!("使用Rust开发hdd命令行工具，敬请期待");
+    let yaml_str = include_str!("../docker-compose.yml");
+    let result: Server = serde_yaml::from_str(yaml_str).unwrap();
+    println!("{:#?}", result);
 }
