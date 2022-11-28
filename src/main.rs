3db4543401204e8b524bc02ff9708a26d31c3cd9
wjun <wjunjobs@outlook.com>
2022-11-28 15:35:33 +0800

    测试rust读写yaml文件

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate serde_yaml;
 extern crate serde;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 use serde::{Serialize, Deserialize};
 
 
@@ -26,4 +28,6 @@ fn main() {
     let yaml_str = include_str!("../docker-compose.yml");
     let result: Server = serde_yaml::from_str(yaml_str).unwrap();
     println!("{:#?}", result);
+    let result1 = serde_yaml::to_string(&result).unwrap();
+    File::create("test.yml").unwrap().write(&result1.as_bytes()).unwrap();
 }
